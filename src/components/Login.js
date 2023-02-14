69d6286cd56df7d6d3d8ee2af4046cd5c9a26ad4
amirthenew <ah.abshari@hotmail.com>
2023-02-14 22:29:14 +0330

    login page isnt empty now !

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+//icon
+import google from '../assets/google.svg'
 
 const Login = () => {
     return (<div>
-        <h1>
-        Login
-        </h1>
+<div>
+<h2>Welcome to Webichat!</h2>
+
+<div>
+<img alt='google' src={google} style={{width:'50px'}}/> sign in with google
+</div>
+</div>
         </div>  );
 }
  
