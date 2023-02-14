cefa0a7815233b1e63958549114b8e9f38150bed
amirthenew <ah.abshari@hotmail.com>
2023-02-14 23:03:04 +0330

    login styled

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,17 @@ import React from 'react';
 //icon
 import google from '../assets/google.svg'
 
+import styles from './Login.module.css'
+
 const Login = () => {
-    return (<div>
-<div>
+    return (
+        
+        <div className={styles.loginPage}>
+<div className={styles.loginCard}>
 <h2>Welcome to Webichat!</h2>
 
-<div>
-<img alt='google' src={google} style={{width:'50px'}}/> sign in with google
+<div className={styles.button}>
+<img alt='google' src={google}/> sign in with google
 </div>
 </div>
         </div>  );
