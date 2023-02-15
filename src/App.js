f70af7c7cd3092beb2050df182b3b2179959bce5
amirthenew <ah.abshari@hotmail.com>
2023-02-15 23:26:10 +0330

    chat js created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import { Switch,Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Chats from './components/Chats';
+
+
+import AuthContextProvider from './contexts/AuthContextProvider';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-      <Route path='/' component={Login}>
-      </Route>
+    <AuthContextProvider>
+          <Switch>
+          <Route path='/chats' component={Chats}/>
+      <Route path='/' component={Login}/>
       </Switch>
+    </AuthContextProvider>
     </div>
   );
 }
