48be7e139a0eea57fbff994e90d75a3936ce984a
amirthenew <ah.abshari@hotmail.com>
2023-02-16 00:29:56 +0330

    set initial values of chatengine

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import styles from './Chats.module.css'
 import { auth } from '../firebase';
 import { useHistory } from 'react-router-dom';
+import { ChatEngine } from 'react-chat-engine';
 
 const Chats = () => {
 
@@ -17,6 +18,15 @@ const history = useHistory()
 
     return (<div className={styles.container}>
         <Navbar logoutHandler={logoutHandler}/>
+
+        <ChatEngine
+        height='calc(100vh - 50px)'
+        projectID='ab189720-9648-46d3-bd2f-5b2e11c172f3'
+        userName='.'
+        userSecret='.'
+        
+
+        />
         </div>  );
 }
  
