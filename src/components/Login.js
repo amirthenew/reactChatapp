0674a689850b82dda2a42ced3c7e3b104f357dcd
amirthenew <ah.abshari@hotmail.com>
2023-02-15 23:08:16 +0330

    fix styles problem

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import  firebase  from 'firebase/app';
 import { auth } from '../firebase';
-
-//icon
+//icon and styles
 import google from '../assets/google.svg'
-
 import styles from './Login.module.css'
 
 const Login = () => {
@@ -14,7 +12,7 @@ const Login = () => {
 <div className={styles.loginCard}>
 <h2>Welcome to Webichat!</h2>
 
-<div onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
+<div className={styles.button} onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
 <img alt='google' src={google}/> sign in with google
 </div>
 </div>
