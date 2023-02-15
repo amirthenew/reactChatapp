c57ec07fa7074cea7a2f7ff9ff1e4313df663719
amirthenew <ah.abshari@hotmail.com>
2023-02-15 23:17:11 +0330

    usestate useeffect use history

--- src/contexts/AuthContextProvider.js
+++ src/contexts/AuthContextProvider.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import { useHistory } from 'react-router-dom';
+import { auth } from '../firebase';
 
+const AuthContext = React.createContext()
 
 const AuthContextProvider = () => {
+
+const [loading,setLoading]=useState(true)
+const [user,setUser] = useState('')
+const history = useHistory()
+
+useEffect(()=>{
+auth.onAuthStateChanged(user=>{
+    setUser(user);
+    setLoading(false)
+    history.push('/chats')
+})
+},[user,history])
+
+
+
     return (<div>
         
         </div>  );
