ff0d4a5989eab441913a2fd1ac5c16cc6ef92585
amirthenew <ah.abshari@hotmail.com>
2023-02-15 23:27:26 +0330

    navbar created

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Navbar = () => {
+    return (<div>
+        <div>
+        Webichat
+        </div>
+        <div>
+        Logout
+        </div>
+        </div>  );
+}
+ 
+export default Navbar;
